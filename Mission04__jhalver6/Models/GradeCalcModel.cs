855dda46ecf2bb826a7ec5370d00f464dadf4f3c
jhalver6 <jhalver6@byu.edu>
2023-02-06 17:09:11 -0700

    GradeCalc

--- Mission04__jhalver6/Models/GradeCalcModel.cs
+++ Mission04__jhalver6/Models/GradeCalcModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,29 @@ namespace Mission04__jhalver6.Models
 {
     public class GradeCalcModel
     {
+        [Required(ErrorMessage ="The Assignments grade is required")]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
         public int assignment { get; set; }
+
+        [Required(ErrorMessage = "The Project grade is required")]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
         public int project { get; set; }
+
+
+        [Required(ErrorMessage = "The Quiz grade is required")]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
         public int quiz { get; set; }
+
+        [Required(ErrorMessage = "The Midterm grade is required")]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
         public int midterm { get; set; }
+
+        [Required(ErrorMessage = "The Final grade is required")]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
         public int final { get; set; }
+
+        [Required(ErrorMessage = "The Intex grade is required")]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
         public int intex { get; set; }
     }
 }
